cc18d5f5f57c737e68c507ffe93bfaf8c814a78c
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-16 16:56:33 +0530

    Add files via upload

---
+++ p13p.py
@@ -0,0 +1,6 @@
+# wapp to covert won into rupees
+won = float(input( "enter amount in \u20A9"))
+rupees = 0.063 * won
+
+print(" amount in \u20A9 " , won)
+print(" amount in \u20B9 " , round(rupees,2))
